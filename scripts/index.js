08afa0f055c86fedddadc0b2045f66eab64fd911
solomonbarayev <sbarayev2@gmail.com>
2022-05-26 12:57:39 +0300

    adding Ori's updates

--- scripts/index.js
+++ scripts/index.js
@@ -15,3 +15,19 @@ observer.observe(element);
 
 //adding parrallax scrolling effect with rellaxJS library
 let rellax = new Rellax(".rellax");
+
+const menuButton = document.querySelector(".nav__hmburger-button");
+const menuLinks = document.querySelector(".nav__links");
+const linksList = Array.from(menuLinks.querySelectorAll(".nav__link"));
+const navBar = Array.from(document.querySelectorAll(".nav__bar"));
+const toggleMenuButton = () => {
+  menuLinks.classList.toggle("nav__links_active");
+  menuButton.classList.toggle("nav__hmburger-button_open");
+  navBar.forEach((bar) => {
+    bar.classList.toggle(`${bar.classList[1]}_active`);
+  });
+};
+menuButton.addEventListener("click", () => toggleMenuButton());
+linksList.forEach((link) => {
+  link.addEventListener("click", () => toggleMenuButton());
+});
